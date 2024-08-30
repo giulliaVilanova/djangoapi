29dafb243b71cd351f5f6df1a5b75388f42faa01
giulliaVilanova <107008118+giulliaVilanova@users.noreply.github.com>
2024-08-29 21:33:50 -0300

    Delete Django API/api directory

--- Django API/api/djangoapi/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('users/', views.UserListCreate.as_view(), name='user-view-create'),
-    path('users/<int:pk>/', views.UserRetrieveUpdateDestroy.as_view(), name='update'),
-]
